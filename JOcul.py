57f8c50692280f20e65095db4edbdcca1ab6aec2
Barbinta Andrei <barbintaa@yahoo.com>
2020-04-16 15:28:47 +0200

    Alta o sa o facem si mai lata

--- JOcul.py
+++ JOcul.py
@@ -1,7 +1,17 @@
 import pygame
 from pygame.locals import *
 
+movex = 0
+movey = 0
+px = 480
+py = 200
+
 screen = pygame.display.set_mode((800, 600))
+
+pimg = pygame.image.load ('eat.png')
+
+def player(x, y):
+    screen.blit(pimg, ( x, y ))
 
 pygame.display.set_caption('BarBy')
 icon = pygame.image.load('pizza.png')
@@ -10,8 +20,37 @@ pygame.display.set_icon(icon)
 
 running = True
 while running:
+    screen.fill((102, 153, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((102, 153, 0))   
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_LEFT:
+                movex -= 0.1
+            if event.key == K_RIGHT:
+                movex =+ 0.1
+            if event.key == K_UP:
+                movey -= 0.1
+            if event.key == K_DOWN:
+                movey += 0.1
+        if event.type == pygame.KEYUP:
+            if event.key == K_LEFT or event.key == K_RIGHT:
+                movex = 0
+            if event.key == K_UP or event.key == K_DOWN:
+                movey = 0   
+         
+    px += movex
+    py += movey   
+
+    if px <= 0:
+        px = 0 
+    elif px >= 736:
+        px = 736      
+    if py <= 0:
+        py = 0 
+    elif py >= 536:
+        py = 536        
+
+    player(px, py)  
     pygame.display.update()     
