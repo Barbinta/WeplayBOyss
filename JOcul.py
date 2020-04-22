749f8637300f5a76f5c6572b3ec4cfb8069ab529
Barbinta Andrei <barbintaa@yahoo.com>
2020-04-22 17:54:38 +0200

    cod

--- JOcul.py
+++ JOcul.py
@@ -3,15 +3,31 @@ import pygame
 import math
 from pygame.locals import *
 
-A = 5
-movex = 0
-movey = 0
-px = 480
-py = 200
+A = 2
+B = False
+C = True
+
+movepx = 0
+movepy = 0
+
+movebx = 0
+moveby = 0
+
+movegy = 0.5
+
+px = 368
+py = 400
+
+gx = px
+gy = 400
+
 mx = random.randint(32, 768) 
 my = random.randint(32, 568)
+
 bx = random.randint(32, 768)
-by = random.randint(32, 768)
+by = random.randint(32, 168)
+
+
 
 screen = pygame.display.set_mode((800, 600))
 
@@ -21,7 +37,7 @@ ming = pygame.image.load ( 'cookie.png' )
 
 def coll(plx, ply, mnx, mny):
     distance = math.sqrt(( math.pow( plx - mnx, 2 ) ) + math.pow( ply - mny, 2 ) )
-    if distance < 32:
+    if distance < 36:
         return True
         
 def boss(x, y):
@@ -33,6 +49,12 @@ def player(x, y):
 def manc(x, y):
     screen.blit(ming, ( x, y ))
 
+def shoot(x, y):
+    global B 
+    B = True
+    screen.blit(ming, ( x+16, y ))
+
+
 pygame.display.set_caption('BarBy')
 icon = pygame.image.load('pizza.png')
 pygame.display.set_icon(icon)
@@ -41,27 +63,60 @@ pygame.display.set_icon(icon)
 running = True
 while running:
     
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        if A == 0 and B == False:
+            if event.type == pygame.KEYDOWN:
+                if event.key == K_SPACE:
+                    gx = px
+                    shoot( gx, gy )
 
         if event.type == pygame.KEYDOWN:
             if event.key == K_LEFT:
-                movex -= 0.3
+                movepx -= 0.3
             if event.key == K_RIGHT:
-                movex =+ 0.3
-            if event.key == K_UP:
-                movey -= 0.3
-            if event.key == K_DOWN:
-                movey += 0.3
+                movepx =+ 0.3
+            if A != 0:
+                if event.key == K_UP:
+                    movepy -= 0.3
+            if A != 0:
+                if event.key == K_DOWN:
+                    movepy += 0.3
         if event.type == pygame.KEYUP:
             if event.key == K_LEFT or event.key == K_RIGHT:
-                movex = 0
+                movepx = 0
             if event.key == K_UP or event.key == K_DOWN:
-                movey = 0   
-         
-    px += movex
-    py += movey   
+                movepy = 0
+        
+            
+            
+    px += movepx
+    py += movepy 
+
+    if A == 0:
+        screen.fill((255, 255, 0))
+        boss( bx, by )
+        
+        if B == True:
+            shoot( gx, gy )
+            gy -= movegy
+        if C :
+            C = False
+            px = 368
+            py = 400
+        if gy == 0:
+            B = False
+            gy = 400
+        if coll(gx, gy, bx, by):
+            B = False
+            gy = 400
+             
+
+        
 
     if px <= 0:
         px = 0 
@@ -72,20 +127,16 @@ while running:
     elif py >= 536:
         py = 536        
 
-     
+
     if A != 0:
         screen.fill((102, 153, 0))
         manc(mx, my)
         if coll(px, py, mx, my):
             mx = random.randint(32, 768) 
             my = random.randint(32, 568)
-            A -= 1
+            A -= 1   
 
-    if A == 0:
-        screen.fill((255, 255, 0))
-        boss(bx, by)
         
-
-    
+         
     player(px, py)
     pygame.display.update()     
